aa3a548f0e1a4e9eba4634acfecdaed51a05c8ec
Matt Cowski <mattcowski@gmail.com>
2013-08-27 09:57:55 -0500

    branch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MooChatStarter::Application.config.secret_key_base = 'a3d7ace8b42e218bcc9d8e986e7472dfe7e7938943f8524a97a6268732b6b8bce2396d1a4b8535dcb539a355c473e16469b04ce70db9a5a63adceee5088164d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MooChatStarter::Application.config.session_store :cookie_store, key: '_MooChat_Starter_session'
